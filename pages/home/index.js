9f45958f3851f9589bd2699152f5cd4c69609654
Leandro Lopes <leandrolopes.tinfem2023@gmail.com>
2025-09-03 17:49:24 -0300

    styling changes, fixed loggedinFtech

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
-    var root = document.querySelector(':root');
+    // const header_height = document.querySelector('header').offsetHeight;
+    // document.querySelector(':root').style.setProperty('--content-offset', `${header_height}`);
 
     setUserButtons(window.getCookie('userInfo'));
     setTheme(window.getCookie('darkMode'));
@@ -9,10 +10,10 @@ async function setTheme(modeCookie)
 {
     if(modeCookie !== undefined)
     {
-        document.body.style.setProperty('--background', '#999');
-        document.body.style.setProperty('--light', '#aaa');
-        document.body.style.setProperty('--dark', '#666');
-        document.body.style.setProperty('--text', '#000');
+        // document.body.style.setProperty('--background', '#999');
+        // document.body.style.setProperty('--light', '#aaa');
+        // document.body.style.setProperty('--dark', '#666');
+        // document.body.style.setProperty('--text', '#000');
     }
 }
 
@@ -23,8 +24,13 @@ async function setUserButtons(userInfo)
         document.querySelector('.user-buttons').innerHTML += 
         '<button onclick="window.goTo(\'register\')" class="roboto-heavy user-button">REGISTER</button> \
         <button onclick="window.goTo(\'login\')" class="roboto-heavy user-button">LOGIN</button>'
-        
-        document.querySelector('.content').innerHTML += (await (await fetch('api/home/loggedIn.html')).bytes()).toString('utf-8');
+
+        fetch('api/home/loggedIn.html').then(function (response) {
+            return response.text()
+        })
+        .then(function (res) {
+            document.querySelector('.content').innerHTML += res;
+        });
     }
     else
     {
